c8e2bee24593540453f8358e2987f75f82c865aa
Bruno Horta <brunohorta82@gmail.com>
2017-07-06 00:42:38 +0100

    melhoramentos

--- Firmware/Firmware_mqtt.ino/Firmware_mqtt.ino.ino
+++ Firmware/Firmware_mqtt.ino/Firmware_mqtt.ino.ino
@@ -1,16 +1,22 @@
 #include <PubSubClient.h>
-#define BUFFER_SIZE 100
-#define STREET_DOOR 5
-#define GARAGE_DOOR 4
 //Wi-Fi Manger library
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>//https://github.com/tzapu/WiFiManager
+#define STREET_DOOR 5
+#define GARAGE_DOOR 4
+#define BUFFER_SIZE 100
+#define STREET_DOOR_BUSY_TIME  21000
+#define GARAGE_DOOR_BUSY_TIME 21000
+
 // Update these with values suitable for your network.
 IPAddress server(192, 168,187,203);
 
 String garagedoorState = "UNDEFINED";
 String streetdoorState = "UNDEFINED";
+
+int delayGarageDoorTimeToOpenClose = 0;
+int delayStreetDoorTimeToOpenClose = 0;
 
 WiFiClient wclient;
 PubSubClient client(wclient, server);
@@ -29,27 +35,38 @@ void setup() {
 
 
 void handleStreetDoor(String action) {
-  if(garagedoorState == "UNDEFINED"){
+  if(garagedoorState == "UNDEFINED" || delayStreetDoorTimeToOpenClose > millis()){
     return; 
    }
+   
    if(streetdoorState != action){
-    garagedoorState = action;
+    streetdoorState = action;
     digitalWrite(STREET_DOOR, HIGH);
     delay(2000);
     digitalWrite(STREET_DOOR, LOW);
+    delayStreetDoorTimeToOpenClose = millis() + STREET_DOOR_BUSY_TIME;
    }
 }
 
 void handleGarageDoor(String action) {
-  if(garagedoorState == "UNDEFINED"){
+  if(delayGarageDoorTimeToOpenClose > millis()){
+    Serial.println("Busy");
+    Serial.println(garagedoorState);
+    }else{
+      Serial.println("ready");
+      Serial.println(garagedoorState);
+      }
+  if(garagedoorState == "UNDEFINED" || delayGarageDoorTimeToOpenClose > millis()){
     return; 
    }
    
    if(garagedoorState != action){
     garagedoorState = action;
+    
     digitalWrite(GARAGE_DOOR, HIGH);
     delay(2000);
     digitalWrite(GARAGE_DOOR, LOW);
+    delayGarageDoorTimeToOpenClose = millis() + GARAGE_DOOR_BUSY_TIME;
    }
 }
 
@@ -71,15 +88,19 @@ void callback(const MQTT::Publish&pub) {
 
 
 void loop() {
+
   if (WiFi.status() == WL_CONNECTED) {
     if (!client.connected()) {
-      if (client.connect(MQTT::Connect("GarageDoorController")
+      if (client.connect(MQTT::Connect("GarageDoorControll")
       .set_auth("moscas", "moscasMoscas82"))) {
+         Serial.println("Connected to MQTT server");
         client.subscribe("home-assistant/garagedoor/set");
         client.subscribe("home-assistant/streetdoor/set");
         client.subscribe("home-assistant/garagedoor");  
         client.subscribe("home-assistant/streetdoor");   
-      }
+      }else{
+        Serial.println("MQTT ERROR!");
+        }
     }
 
     if (client.connected()){
--- Firmware/Sensor/Sensor.ino
+++ Firmware/Sensor/Sensor.ino
@@ -1,51 +1,42 @@
-#include <MQTT.h>
 #include <PubSubClient.h>
-#include <ESP8266WiFi.h>;
+//Wi-Fi Manger library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>//https://github.com/tzapu/WiFiManager
 #define BUFFER_SIZE 100
 #define SENSOR_CLOSE 4
 
-
-const char *ssid =  "Bruno's Wi-Fi Network";    // cannot be longer than 32 characters!
-const char *pass =  "PW";    //
-
 // Update these with values suitable for your network.
-
-IPAddress server(192, 168,187,70);
+IPAddress server(192, 168,187,203);
 
 String notificationState = "UNDEFINED";
-String readState = "UNDEFINED";  
 long lastNotifTime = 0;
 long notifDelay = 2000;
 WiFiClient wclient;
 PubSubClient client(wclient, server);
 
 void setup() {
-  //Ativar apenas modo estaÃ§Ã£o
-  WiFi.mode(WIFI_STA);
-  pinMode(SENSOR_CLOSE,INPUT);
   // Setup console
   Serial.begin(115200);
-  delay(10);
+  WiFiManager wifiManager;
+  //reset saved settings
+//wifiManager.resetSettings();
+  wifiManager.autoConnect("GarageDoorSensor");
+  pinMode(SENSOR_CLOSE,INPUT);
+ 
 }
-
+String realState = "UNDEFINED";
 void loop() {
 
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Connecting to ");
-    Serial.print(ssid);
-    Serial.println("...");
-    WiFi.begin(ssid, pass);
-
-    if (WiFi.waitForConnectResult() != WL_CONNECTED)
-      return;
-    Serial.println("WiFi connected");
-  }
 
   if (WiFi.status() == WL_CONNECTED) {
     if (!client.connected()) {
       if (client.connect(MQTT::Connect("GarageDoorSensor")
       .set_auth("moscas", "moscasMoscas82"))) {
         Serial.println("Connected to MQTT server");
+        if(realState != "UNDEFINED"){
+          client.publish_P("home-assistant/garagedoor",realState.c_str(),realState.length(),true);
+        }
       }else{
         Serial.println("MQTT ERROR!");
         }
@@ -54,19 +45,21 @@ void loop() {
     if (client.connected()){
       client.loop();
     }
-    //FAIL SAFE TO RESOLVE LIMITATION Of millis() funtion
-  if(millis() < lastNotifTime){
-    lastNotifTime = 0;
-    }
-  if ((millis() - lastNotifTime) > notifDelay) {
-      int sensorVal = digitalRead(SENSOR_CLOSE);
-    lastNotifTime = millis();
-    if(sensorVal){
-       client.publish("home-assistant/garagedoor","LOCK");
-    }else{
-     client.publish("home-assistant/garagedoor","UNLOCK");
-    }
   
+  //CORRECT WAY TO ROLLOVER millis() FUNCTION
+  unsigned long currentMillis = millis();
+  if ((unsigned long)(currentMillis - lastNotifTime) >= notifDelay) {
+    int sensorVal = digitalRead(SENSOR_CLOSE);
+    lastNotifTime = currentMillis;
+    if(sensorVal){
+      realState = "LOCK";
+    }else{
+      realState = "UNLOCK";     
+    }
+    if(realState != notificationState){
+      client.publish_P("home-assistant/garagedoor",realState.c_str(),realState.length(),true);
+      notificationState = realState;
+      }
   }
  
 }
